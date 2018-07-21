65b58ce8ffda99c951e4983904721ade7ed8ab16
Parthiv Ganguly <38831405+parthiv1024@users.noreply.github.com>
2018-07-21 10:52:03 +0530

    Update twitter_sentiment_analysis.py

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -3,11 +3,11 @@ from textblob import TextBlob
 import csv
 
 # Step 1 - Authenticate
-consumer_key = '5jFdQPbGtWkz31vXl0HQW4WPk'
-consumer_secret = 'w8JFsbFsvl4K0hh5xXqssAu8S87wTe0SNEVq1r9RPfP0VUNmhS'
+consumer_key = 'YOUR CONSUMER KEY'
+consumer_secret = 'YOUR CONSUMER SECRET'
 
-access_token = '971375935940386816-adY0Y2rwvfdMwh4YvbrZsDA0Jz3HFp1'
-access_token_secret = 'mIZrzhggUbmuGCE656QCHOqY6PpnqO6Nf0ChwjjDTyHfR'
+access_token = 'YOUR ACCESS TOKEN'
+access_token_secret = 'YOUR ACCESS TOKEN SECRET'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
